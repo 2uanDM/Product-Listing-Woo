252ca5247241f88b42196a320369e20165c29e3a
2uanDM <hokage321xxx@gmail.com>
2023-06-15 02:10:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,84 @@
-import json
-from urllib.parse import unquote_plus
-response_text = '{'success':true,'data':{'user_id':5,'description':'test','permissions':'read_write','consumer_key':'ck_9bb3410113f2024182396ad1c976ba4092c8d247','consumer_secret':'cs_a6cf473c9b8829490cd02ca132f8a5b0e31e03a7','truncated_key':'2c8d247','message':'API Key generated successfully. Make sure to copy your new keys now as the secret key will be hidden once you leave this page.','revoke_url':'<a style=\'color: #a00; text-decoration: none;\' href=\'https:\/\/silegend.com\/wp-admin\/admin.php?page=wc-settings&#038;tab=advanced&#038;section=keys&#038;revoke-key=11&#038;_wpnonce=609a5f7d81\'>Revoke key<\/a>'}}'
+from utils.woocommerce import API_key
 
+test = API_key(
+    url='silegend.com',
+    cookies=[
+        {
+            "domain": "silegend.com",
+            "hostOnly": True,
+            "httpOnly": False,
+            "name": "wordpress_test_cookie",
+            "path": "/",
+            "sameSite": None,
+            "secure": True,
+            "session": True,
+            "storeId": None,
+            "value": "WP%20Cookie%20check"
+        },
+        {
+            "domain": "silegend.com",
+            "expirationDate": 1718300231.961251,
+            "hostOnly": True,
+            "httpOnly": False,
+            "name": "wp-settings-time-5",
+            "path": "/",
+            "sameSite": None,
+            "secure": True,
+            "session": False,
+            "storeId": None,
+            "value": "1686764231"
+        },
+        {
+            "domain": "silegend.com",
+            "hostOnly": True,
+            "httpOnly": False,
+            "name": "tk_ai",
+            "path": "/",
+            "sameSite": None,
+            "secure": False,
+            "session": True,
+            "storeId": None,
+            "value": "woo%3ATexhoAaP4f0LVgtS%2BoQmIANV"
+        },
+        {
+            "domain": "silegend.com",
+            "hostOnly": True,
+            "httpOnly": True,
+            "name": "wordpress_sec_091348d5935107ab5f95c23ec9437605",
+            "path": "/wp-admin",
+            "sameSite": None,
+            "secure": True,
+            "session": True,
+            "storeId": None,
+            "value": "teamkhacanh%7C1686937030%7CBaQM4DKjQFOcCexQQBX5xa3WFKA8Jl1zDvkFZeDwqzR%7C7590361b35e9d2bc7f89ccc84744fedd0fab81ec15f63b95a4fbd7cf7788f334"
+        },
+        {
+            "domain": "silegend.com",
+            "expirationDate": 1718300231.96123,
+            "hostOnly": True,
+            "httpOnly": False,
+            "name": "wp-settings-5",
+            "path": "/",
+            "sameSite": None,
+            "secure": True,
+            "session": False,
+            "storeId": None,
+            "value": "libraryContent%3Dbrowse"
+        },
+        {
+            "domain": "silegend.com",
+            "hostOnly": True,
+            "httpOnly": True,
+            "name": "wordpress_logged_in_091348d5935107ab5f95c23ec9437605",
+            "path": "/",
+            "sameSite": None,
+            "secure": True,
+            "session": True,
+            "storeId": None,
+            "value": "teamkhacanh%7C1686937030%7CBaQM4DKjQFOcCexQQBX5xa3WFKA8Jl1zDvkFZeDwqzR%7Cb62e28504320e114ab67de33e13796bcc9478c4375c2d99b0d9ac9806ef1aeb8"
+        }
+    ]
+)
 
-# Create a new variable as the raw string literal of response_text
-
-start_revoke_url_index = response_text.find('https')
-end_revoke_url_index = response_text.find('>Revoke key')
-
-response_text = response_text[start_revoke_url_index:end_revoke_url_index-1]
-
-decoded_url = response_text.replace('\\/', '/').replace('&#038;', '&')
-
-print(decoded_url)
+test.create()
+print(test)
